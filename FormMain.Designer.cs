4c9fa656fe4f25a3c0ea65c80c0b3e33d08c3bd4
Inga <inganiscior@gmail.com>
2019-04-07 18:20:54 +0200

    Full game 1

---
+++ FormMain.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace IngaNisciorZadLab1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonBottom = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textTimer = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listOfItemsTextBox = new System.Windows.Forms.RichTextBox();
+            this.endOfGameButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(12, 200);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(89, 47);
+            this.buttonLeft.TabIndex = 0;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(743, 200);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(97, 47);
+            this.buttonRight.TabIndex = 1;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(361, 9);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(96, 43);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.Text = "^";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonBottom
+            // 
+            this.buttonBottom.Location = new System.Drawing.Point(361, 536);
+            this.buttonBottom.Name = "buttonBottom";
+            this.buttonBottom.Size = new System.Drawing.Size(96, 53);
+            this.buttonBottom.TabIndex = 3;
+            this.buttonBottom.Text = "v";
+            this.buttonBottom.UseVisualStyleBackColor = true;
+            this.buttonBottom.Click += new System.EventHandler(this.buttonBottom_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(33, 536);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 43);
+            this.buttonStart.TabIndex = 8;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click_1);
+            // 
+            // textTimer
+            // 
+            this.textTimer.Location = new System.Drawing.Point(702, 9);
+            this.textTimer.Name = "textTimer";
+            this.textTimer.Size = new System.Drawing.Size(85, 22);
+            this.textTimer.TabIndex = 9;
+            this.textTimer.Text = "1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(514, 128);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 157);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(216, 310);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(158, 170);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(642, 381);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(145, 172);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 55);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listOfItemsTextBox
+            // 
+            this.listOfItemsTextBox.Location = new System.Drawing.Point(968, 9);
+            this.listOfItemsTextBox.Name = "listOfItemsTextBox";
+            this.listOfItemsTextBox.Size = new System.Drawing.Size(150, 580);
+            this.listOfItemsTextBox.TabIndex = 10;
+            this.listOfItemsTextBox.Text = "";
+            this.listOfItemsTextBox.TextChanged += new System.EventHandler(this.listOfItemsTextBox_TextChanged);
+            // 
+            // endOfGameButton
+            // 
+            this.endOfGameButton.Location = new System.Drawing.Point(12, 9);
+            this.endOfGameButton.Name = "endOfGameButton";
+            this.endOfGameButton.Size = new System.Drawing.Size(908, 580);
+            this.endOfGameButton.TabIndex = 11;
+            this.endOfGameButton.Text = "button1";
+            this.endOfGameButton.UseVisualStyleBackColor = true;
+            this.endOfGameButton.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 626);
+            this.Controls.Add(this.endOfGameButton);
+            this.Controls.Add(this.listOfItemsTextBox);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textTimer);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonBottom);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonBottom;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textTimer;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox listOfItemsTextBox;
+        private System.Windows.Forms.Button endOfGameButton;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace IngaNisciorZadLab1
+{
+
+    public partial class FormMain : Form
+    {
+        //licznik
+        int[] counter = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        String[] pictures ={"rocks.png" , "rope.png", "leaf.png", "axe.png", "squirrel.png" , "match.png",
+            "knife.png", "cup.png", "fork.png", "ballon.png","money.png", "stick.png" };
+        int actions = 0;
+        Boolean clicked = false;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+        }
+        /// <summary>
+        /// Przycisk przenoszacy na planszy w dol
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonBottom_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                ramdomItems();
+                actions++;
+            }
+
+            if (!clicked)
+            {
+                buttonRight.Visible = false;
+                buttonLeft.Visible = false;
+                buttonBottom.Visible = false;
+            }
+            else
+            {
+                buttonRight.Visible = true;
+                buttonLeft.Visible = true;
+                buttonUp.Visible = true;
+            }
+            clicked = !clicked;
+
+            
+        }
+        /// <summary>
+        /// przycisk przenoszacy na planszy w gore
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                ramdomItems();
+                actions++;
+            }
+            
+            if (!clicked)
+            {
+                buttonRight.Visible = false;
+                buttonLeft.Visible = false;
+                buttonUp.Visible = false;
+            }
+            else
+            {
+                buttonRight.Visible = true;
+                buttonLeft.Visible = true;
+                buttonBottom.Visible = true;
+            }
+            clicked = !clicked;
+
+
+           
+        }
+        /// <summary>
+        /// przycisk przenoszacy na planszy w prawo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                ramdomItems();
+                actions++;
+            }
+            
+
+            if (!clicked)
+            {
+                buttonRight.Visible = false;
+                buttonBottom.Visible = false;
+                buttonUp.Visible = false;
+            }
+            else
+            {
+                buttonLeft.Visible = true;
+                buttonBottom.Visible = true;
+                buttonUp.Visible = true;
+            }
+            clicked = !clicked;
+
+
+            
+        }
+        /// <summary>
+        /// przycisk przenoszacy na planszy w lewo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                ramdomItems();
+                actions++;
+            }
+
+            if (!clicked)
+            {
+                buttonLeft.Visible = false;
+                buttonBottom.Visible = false;
+                buttonUp.Visible = false;
+            }
+            else
+            {
+                buttonRight.Visible = true;
+                buttonBottom.Visible = true;
+                buttonUp.Visible = true;
+            }
+            clicked = !clicked;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textTimer.Text = (timer1.Interval).ToString();
+            timer1.Interval += 1;
+            if ((timer1.Interval % 100) == 1)
+            {
+                ramdomItems();
+
+            }
+            if (actions >= 20)
+            {
+                endOfGameButton.Visible = true;
+                timer1.Stop();
+                if (endOfGame())
+                {
+                    endOfGameButton.Text = "WYGRALES";
+                }
+                else
+                {
+                    endOfGameButton.Text = "PRZEGRALES";
+                }
+            }
+        }
+
+        private void buttonStart_Click_1(object sender, EventArgs e)
+        {
+
+            timer1.Interval = 1;
+            timer1.Start();
+
+            refreshList();
+        }
+        Random random = new Random(3000);
+        private void ramdomItems()
+        {
+            int n = random.Next(2, 5);
+            pictureBox1.Visible = (n >= 1);
+            pictureBox2.Visible = (n >= 2);
+            pictureBox3.Visible = (n >= 3);
+            pictureBox4.Visible = (n >= 4);
+            if (n >= 1)
+            {
+                int m = random.Next(0, 11);
+                pictureBox1.Image = Image.FromFile(@"..\..\Pictures\" + pictures[m]);
+                pictureBox1.Tag = pictures[m];
+            }
+            if (n >= 2)
+            {
+                int m = random.Next(0, 11);
+                pictureBox2.Image = Image.FromFile(@"..\..\Pictures\" + pictures[m]);
+                pictureBox2.Tag = pictures[m];
+
+            }
+            if (n >= 3)
+            {
+                int m = random.Next(0, 11);
+                pictureBox3.Image = Image.FromFile(@"..\..\Pictures\" + pictures[m]);
+                pictureBox3.Tag = pictures[m];
+            }
+            if (n >= 4)
+            {
+                int m = random.Next(0, 11);
+                pictureBox4.Image = Image.FromFile(@"..\..\Pictures\" + pictures[m]);
+                pictureBox4.Tag = pictures[m];
+            }
+        }
+        public void takeItems(String nameOfItem)
+        {
+            int index = Array.IndexOf(pictures, nameOfItem);
+            counter[index]++;
+            refreshList();
+            actions++;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            string imageAdress = pictureBox2.Tag.ToString();
+            takeItems(imageAdress);
+            pictureBox2.Visible = false;
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            string imageAdress = pictureBox4.Tag.ToString();
+            takeItems(imageAdress);
+            pictureBox4.Visible = false;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            string imageAdress = pictureBox3.Tag.ToString();
+            takeItems(imageAdress);
+            pictureBox3.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string imageAdress = pictureBox1.Tag.ToString();
+            takeItems(imageAdress);
+            pictureBox1.Visible = false;
+        }
+
+        private void listOfItemsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void refreshList()
+        {
+            string result = "";
+            for (int i = 0; i < 12; i++)
+            {
+                result += pictures[i].Substring(0, pictures[i].Length - 4);
+                result += " : " + counter[i] + '\n';
+            }
+            listOfItemsTextBox.Text = result;
+        }
+        private Boolean endOfGame()
+        {
+            int more = 0;
+            string result = "\n Przedmioty przeciwnika:\n";
+            for (int i = 0; i < 12; i++)
+            {
+                int enemy = random.Next(0, 4);
+                result += pictures[i].Substring(0, pictures[i].Length - 4);
+                result += " : " + enemy + '\n';
+                if (counter[i] >= enemy)
+                {
+                    more++;
+                }
+
+
+            }
+            listOfItemsTextBox.Text += result;
+            if (more > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
+
